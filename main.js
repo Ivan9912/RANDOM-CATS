f678e0c47e212a6905b35bcc83653b36ce2210e7
Ivan Maidana <ivan.maidana9912@gmail.com>
2022-11-25 16:40:38 -0300

    Refacts

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-console.log('Hello, cat lover')
-const buttonImage = document.querySelector('#btn')
-const img = document.querySelector('img')
-buttonImage.addEventListener('click', refreshImage)
-const URL = 'https://api.thecatapi.com/v1/images/search';
-img.src = `https://i.imgur.com/ouV024z.jpg`
+console.log('Hello, cat lover');
+const buttonImage = document.querySelector('#btn');
+const img = document.querySelector('img');
+buttonImage.addEventListener('click', reloadImage);
+const API_URL = 'https://api.thecatapi.com/v1/images/search';
+reloadImage();
 
-async function refreshImage () {
-    const response = await fetch(URL)
-    const data = await response.json()
-    img.src = data[0].url
-}
+async function reloadImage () {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    img.src = data[0].url;
+};
